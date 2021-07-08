8e1c4a598c3099524323f42aec2fb9754ecd83c8
Minh Trí <30529020+minhtri31@users.noreply.github.com>
2021-07-08 18:30:56 +0700

    Update time.js

--- js/time.js
+++ js/time.js
@@ -38,5 +38,5 @@ function displayClock() {
   document.getElementById('month').innerText = mm;
   document.getElementById('day').innerText = dd;
 
-  setTimeout(displayClock, 1824);
+  setTimeout(displayClock, 0629);
 }
